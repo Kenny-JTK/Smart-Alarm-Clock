4b9a00e3321fa75cd1bad724e29d2989434ee2d5
Kenny <Kenny.stoepel@telenet.be>
2017-12-24 17:14:44 +0100

    Update

---
+++ Smart Alarm Clock/public/javascripts/alarm.js
@@ -0,0 +1,11 @@
+﻿function myAlarm(Alarm)
+{
+    var checkBox = document.getElementById("Alarm1");
+    console.log(Alarm);
+
+    if (checkBox.checked == true) {
+        prompt("checked");
+    } else {
+        prompt("Unchecked");
+    }
+}
--- Smart Alarm Clock/public/javascripts/time.js
+++ Smart Alarm Clock/public/javascripts/time.js
@@ -5,9 +5,12 @@
     var s = today.getSeconds();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
+    document.getElementById('Time').innerHTML =
+    h + ":" + m +":"+s;
     var t = setTimeout(startTime, 500);
+    var Days = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
+    var Months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+    document.getElementById('Day').innerHTML = Days[today.getDay()] + " " + today.getDate()+ " " + Months[today.getMonth()] + " " + today.getFullYear();
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
