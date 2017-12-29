8a6037d13db664e406ddb8a347ce11a325b991dd
Kenny <Kenny.stoepel@telenet.be>
2017-12-29 13:43:05 +0100

    update with early trigger

--- Smart Alarm Clock/README.md
+++ Smart Alarm Clock/README.md
@@ -3,20 +3,55 @@
 IFTTT smart alarm clock 
 
 2 seperate alarm timers with andriod style time picker
-plays webradio/stream/mp3 and send out a IFTTT trigger at alarm time so you can do whatever you want on IFTTT side.
+plays webradio/stream/mp3 and send out a IFTTT trigger (webhooks) at alarm time so you can do whatever you want on the other side.
 webradio ramps up volume after start ( timing can be changed in settings)
 for IFTTT, create an applet with the trigger being a webhook. copy the webhook links in the config file.
 this is created with the idea of running it on Raspberry pi with a 7" touchscreen (use chromium in kiosk mode)
-at the bottom of the screen there are 3 custom buttons which all 3 can have a different IFTTT event.
+at the bottom of the screen there are 3 custom buttons which all 3 can have a different webhook event.
+
+I started this because i do not want to go around and change my alarm clock in multiple places/apps.
+Also for me the WAF-factor is important as it doesn't require any skills to set an alarm once everything is configured'
+
 
 This is a work in progress, code needs cleaning up, need backup in case of no internet connection.
-all settings can be made in settings/config.json
+Don't blame me if it doesn't wake you up on time!!
 
-all graphic options need to be changed in the css files. 
 
 if you like to contribute, please do. 
+
+
+Configuration :
+
+-all settings can be made in settings/config.json
+-Copy your IFTTT or other webhook links to the config.json file and add up to 3 value to be passed in the webhook POST.
+-for the radio there is only 1 url for both alarms
+-the webradio_fadetime is expressed in ms -> 1s=1000ms
+-the early trigger is exceuted before the alarm triggers. how much before can be set by the the offset value (in minutes)
+-the early trigger can be used to set a nest to heat your home before you wake up or make a cup of coffee at the excact time you need it without setting differnent alarms
+-all graphic options need to be changed in the css files.
+
+access the page by using http://localhost:3000.
+
+
+node run app on startup :
+make sure node runs on startup, for this just add one string at the begining of /etc/rc.local:
+node /full/path/to/myscript.js < /dev/null &
+
+Rpi Chromium kiosk config:
+go to ~/.config/lxsession/LXDE-pi/autostart
+
+delete or comment the line with @xscreensaver -no-splash
+and add
+@xset s off
+@xset -dpms
+@xset s noblank
+@unclutter
+@chromium-browser --noerrdialogs --kiosk --incognito http://www.domain.com/to/kiosk/page
+
 
 To Do 
 
 - have a failsave if internet is down. 
 - Create a settings page for easier customization
+- clean up code
+- improve GUI 
--- Smart Alarm Clock/public/javascripts/time.js
+++ Smart Alarm Clock/public/javascripts/time.js
@@ -1,11 +1,16 @@
-﻿function start() {
+﻿var config ="";
+
+function start()
+{
     $.getJSON("./settings/config.json", function (data) {
-        document.getElementById('Button_left').innerHTML = data.button_left_caption;
-        document.getElementById('Button_center').innerHTML = data.button_center_caption;
-        document.getElementById('Button4').innerHTML = "Play Radio";
-        document.getElementById('Button_right').innerHTML = data.button_right_caption;
-    }); 
-    startTime();
+        config = data;
+    console.log(config);
+    document.getElementById('Button_left').innerHTML = config.button_left_caption;
+    document.getElementById('Button_center').innerHTML = config.button_center_caption;
+    document.getElementById('Button4').innerHTML = "Play Radio";
+    document.getElementById('Button_right').innerHTML = config.button_right_caption;
+    startTime(); 
+    });
 }
 
 
@@ -15,14 +20,18 @@ function startTime() {
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
+    h = checkTime(h);
     m = checkTime(m);
     s = checkTime(s);
 
     document.getElementById('Time').innerHTML = h + ":" + m;
     var t = setTimeout(startTime, 500);
 
+    //var Days = config.Days;
+    //var Months = config.Months;
     var Days = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
     var Months = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+    //console.log(Days);
     document.getElementById('Day').innerHTML = Days[today.getDay()] + " " + today.getDate() + " " + Months[today.getMonth()] + " " + today.getFullYear();
 
     var checkBox1 = document.getElementById("Alarm1");
@@ -33,62 +42,79 @@ function startTime() {
     {
         if ((document.getElementById('Time1').value).split(":")[0] == h && (document.getElementById('Time1').value).split(":")[1] == m && s == "00")
         {
-              // Get Json data from config file
-              $.getJSON("./settings/config.json", function (data) {
-                $.each(data, function (key, val) {
-                    console.log(key + "value:: " + val);
-                    
-                });
-                 // Make IFTTT webhook post
-                $.post(data.AL_1ifttt,
+            // Make IFTTT webhook post
+            $.post(config.AL_1ifttt,
                 {
-                    value1: data.AL_1value1,
-                    value2: data.AL_1value2,
-                    value3: data.AL_1value3
-                },
-                //???
-                function (data, status) {
-                    alert("Data: " + data + "\nStatus: " + status);
-                
+                    value1: config.AL_1value1,
+                    value2: config.AL_1value2,
+                    value3: config.AL_1value3
                 });
-                // Start Music
-                document.getElementById('webradio').volume=0.2;
-                document.getElementById('webradio').play();
-                document.getElementById('Button1').innerHTML = "Stop Radio";
-                fade(data.fadeTime);
-            });
+                
+            // Start Music
+            document.getElementById('webradio').volume=0.2;
+            document.getElementById('webradio').play();
+            document.getElementById('Button1').innerHTML = "Stop Radio";
+            fade(config.fadeTime);
+            
          };
     };
 
     //Alarm 2
     if (checkBox2.checked == true) {
-        if ((document.getElementById('Time2').value).split(":")[0] == h && (document.getElementById('Time2').value).split(":")[1] == m && s == "00") {
-            // Get Json data from config file
-            $.getJSON("./settings/config.json", function (data) {
-                $.each(data, function (key, val) {
-                    console.log(key + "value:: " + val);
-
+        if ((document.getElementById('Time2').value).split(":")[0] == h && (document.getElementById('Time2').value).split(":")[1] == m && s == "00")
+        {
+            // Make IFTTT webhook post
+            $.post(config.AL_2ifttt,
+                {
+                    value1: config.AL_2value1,
+                    value2: config.AL_2value2,
+                    value3: config.AL_2value3
                 });
-                // Make IFTTT webhook post
-                $.post(data.AL_2ifttt,
-                    {
-                        value1: data.AL_2value1,
-                        value2: data.AL_2value2,
-                        value3: data.AL_2value3
-                    },
-                    //???
-                    function (data, status) {
-                        alert("Data: " + data + "\nStatus: " + status);
-
-                    });
-                // Start Music
-                document.getElementById('webradio').volume = 0.2;
-                document.getElementById('webradio').play();
-                document.getElementById('Button4').innerHTML = "Stop Radio";
-                fade(data.fadeTime);
-            });
+                   
+            // Start Music
+            document.getElementById('webradio').volume = 0.2;
+            document.getElementById('webradio').play();
+            document.getElementById('Button4').innerHTML = "Stop Radio";
+            fade(config.fadeTime);
         };
     };
+
+    //early trigger
+    if (config.early_trigger != "" && checkBox1.checked == true || config.early_trigger != "" && checkBox2.checked == true) {
+        var offset = [];
+        offset[0] = math.floor(config.early_offset / 60);
+        offset[1] = config.early_offset - (offset[0] * 60);
+        var alarmTime1 = (document.getElementById('Time1').value).split(":");
+        var alarmTime2 = (document.getElementById('Time2').value).split(":");
+        var earlyAlarm1 = [];
+        var earlyAlarm2 = [];
+        //calculate early alarm 1
+        if (offset[1] <= alarmTime1[1]) {
+            earlyAlarm1[0] = alarmTime1[0] - offset[0];
+            earlyAlarm1[1] = alarmTime1[1] - offset[1];
+        } else {
+            earlyAlarm1[0] = alarmTime1[0] - offset[0]-1;
+            earlyAlarm1[1] = (60 + alarmTime1[1]) - offset[1];
+        };
+        //calculate early alarm 2
+        if (offset[1] <= alarmTime2[1]) {
+            earlyAlarm2[0] = alarmTime2[0] - offset[0];
+            earlyAlarm2[1] = alarmTime2[1] - offset[1];
+        } else {
+            earlyAlarm2[0] = alarmTime2[0] - offset[0] - 1;
+            earlyAlarm2[1] = (60 + alarmTime2[1]) - offset[1];
+        };
+        //trigger alarm
+        if (earlyAlarm1[0] == h && earlyAlarm1[1] == m && "00" == s || earlyAlarm2[0] == h && earlyAlarm2[1] == m && "00" == s) {
+            $.post(config.early_trigger,
+                {
+                    value1: config.early_value1,
+                    value2: config.early_value2,
+                    value3: config.early_value3
+                });
+            console.log("early");
+        };
+    }
 }
 
 // add zero in front of numbers < 10
@@ -124,21 +150,17 @@ function toggleRadio() {
     }
 }
 
-function button_left() {
-    $.getJSON("./settings/config.json", function (data) {
-        $.post(data.button_left, {})
-    });    
+function button_left()
+{
+    $.post(config.button_left, {})   
 }
 
-function button_center() {
-    $.getJSON("./settings/config.json", function (data) {
-        $.post(data.button_center, {})
-    });   
+function button_center()
+{
+    $.post(config.button_center, {})
 }
 
-function button_right() {
-    $.getJSON("./settings/config.json", function (data) {
-        $.post(data.button_right, {})
-    });   
+function button_right()
+{
+    $.post(config.button_right, {})
 }
-
